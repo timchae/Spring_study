02d8c4f401c4d60d85401d5edf06bab8eeaba0de
timchae <timchae2@gmail.com>
2021-07-05 16:40:50 +0900

    logout login 구현

--- springcore/src/main/java/com/sprinhard/springcore/security/WebSecurityConfig.java
+++ springcore/src/main/java/com/sprinhard/springcore/security/WebSecurityConfig.java
@@ -38,6 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
+                .logoutUrl("/user/logout")
                 .permitAll();
     }
 
